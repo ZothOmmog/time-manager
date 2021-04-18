93466ab618ac238acf60439c005366b9e8d40657
ZothOmmog <abpstd@gmail.com>
2021-04-18 21:12:26 +0500

    Applied sider for main page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,17 @@
-import { Button } from 'antd';
 import React from 'react';
 import { DiaryItemCreateForm } from '../components/diary-item-create-form/diary-item-create-form';
 import { DiaryTable } from '../components/diary-table/diary-table';
-import { PageTemplate } from "../components/page-template";
-import { openCreateForm } from '../models/diary-item-create-form/diary-item-create-form-model';
+import { PageTemplate } from '../components/page-template';
+import { SiderTm } from '../components/sider';
 
 export const Home: React.FC<unknown> = () => {
     return (
         <PageTemplate>
+            <SiderTm />
             <>
-                <Button style={{ marginBottom: 16 }} type='primary' onClick={() => openCreateForm()}>Добавить запись</Button>
                 <DiaryTable />
                 <DiaryItemCreateForm />
             </>
         </PageTemplate>
     );
-}
+};
